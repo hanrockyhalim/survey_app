6802326b5a87e9318c28d14de072b1775d96a9be
Hanrocky <hanrocky.halim@gmail.com>
2023-09-24 14:11:03 +0700

    first commit

---
+++ vue/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state: {
+    user: {
+        data: {name: 'Hanrocky'},
+        token: null
+    }
+  },
+  getter: {},
+  actions: {},
+  mutations: {},
+  modules: {},
+});
+
+export default store;
